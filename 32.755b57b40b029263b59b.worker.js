a60996978adfa13136e02f6cf0790dda0d33cab7
windz <windzlife@gmail.com>
2021-08-19 12:47:30 +0800

    Create jbrowse2

---
+++ 32.755b57b40b029263b59b.worker.js
@@ -0,0 +1,2 @@
+this.webpackChunk([32],{1328:function(n,c){}});
+//# sourceMappingURL=32.755b57b40b029263b59b.worker.js.map
---
+++ static/js/45.13ab4599.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@jbrowse/web"]=this["webpackJsonp@jbrowse/web"]||[]).push([[45],{563:function(s,b){}}]);
+//# sourceMappingURL=45.13ab4599.chunk.js.map
