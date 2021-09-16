c89d20da1aad2119e1636a8b2fb35448423dc5c3
windz <windzlife@gmail.com>
2021-09-16 20:37:53 +0800

    feat: jbrowse2 v1.4.4
    update to jbrowse2 v1.4.4
    Add search by gene name

---
+++ 32.ff8da6869c2040672276.worker.js
@@ -0,0 +1,2 @@
+this.webpackChunk([32],{1352:function(n,c){}});
+//# sourceMappingURL=32.ff8da6869c2040672276.worker.js.map
---
+++ static/js/44.afcaea2e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@jbrowse/web"]=this["webpackJsonp@jbrowse/web"]||[]).push([[44],{565:function(s,b){}}]);
+//# sourceMappingURL=44.afcaea2e.chunk.js.map
